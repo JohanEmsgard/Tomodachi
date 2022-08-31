b0740b1731f9ca7cb8780c6bc9eb6dfc9361e116
JohanEmsgard <emsgard.johan@elev.ga.ntig.se>
2022-08-31 13:20:39 +0200

    hbjsdf

--- Tomodachi/Program.cs
+++ Tomodachi/Program.cs
@@ -4,7 +4,15 @@ Console.WriteLine("Welcome to Tamagotchi");
 
 Newclass s1 = new Newclass(); 
 
-Console.WriteLine();
+Console.WriteLine("Pick a name for your Tamagotchi");
+s1.name = Console.ReadLine();
+
+Console.WriteLine($"{s1.name} is a wonderfull name");
+//while (s1.new)
+{
+
+}
+
 
 Console.ReadLine();
 
